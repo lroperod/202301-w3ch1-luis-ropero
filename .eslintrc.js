9f742613b4e540fdcf692af872fdc4683ee5de16
luis <luisrym@gmail.com>
2023-01-30 20:28:55 +0100

    finish configuration

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: 'xo',
+  overrides: [
+    {
+      extends: ['xo-typescript'],
+      files: ['*.ts', '*.tsx'],
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    indent: ['Error', 2],
+    'object-curly-spacing': ['Error', 'Always'],
+  },
+};
